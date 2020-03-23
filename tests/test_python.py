745a7da61b1a17ba308a668e28e100c3d33a5b4c
Matthew Feickert <matthew.feickert@cern.ch>
2020-03-22 22:52:21 -0500

    Correct Python test

--- tests/test_python.py
+++ tests/test_python.py
@@ -7,7 +7,7 @@ def main():
     particles.append(PseudoJet(100.0, 0.0, 0.0, 100.0))  # px, py, pz, E
     particles.append(PseudoJet(150.0, 0.0, 0.0, 150.0))
     R = 0.4
-    jet_def = JetDefinition(antikt_algorithm, R)
+    jet_def = JetDefinition(fastjet.antikt_algorithm, R)
     jets = jet_def(particles)
     print(jet_def)
     for jet in jets:
